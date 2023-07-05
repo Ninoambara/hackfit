2227cae86bd3ab085ecae24bacd915c70676e8db
Nino Ambara Indrawan <ninoambara1@gmail.com>
2023-07-05 21:04:08 +0800

    new

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const filterBtns = document.querySelectorAll(".filter-btn");
+    const contentItems = document.querySelectorAll(".content-item");
+
+    filterBtns.forEach(function (btn) {
+        btn.addEventListener("click", function () {
+            const filter = btn.getAttribute("data-filter");
+
+            filterBtns.forEach(function (btn) {
+                btn.classList.remove("active");
+            });
+            btn.classList.add("active");
+
+            contentItems.forEach(function (item) {
+                if (filter === "all" || item.classList.contains(filter)) {
+                    item.style.display = "block";
+                } else {
+                    item.style.display = "none";
+                }
+            });
+        });
+    });
+});
